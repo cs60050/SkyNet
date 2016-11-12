12db7897e6e29c503e09b232b0093f4cd210ff98
RaptimusPrime <vin9jain@yahoo.co.in>
2016-11-12 13:58:41 +0530

    Added function definitions for reading images and input pipeline

---
+++ utils/feed_input.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+
+def readfile(filename):
+	reader = tf.WholeFileReader()
+	key,value = reader.read(filename)
+	image = tf.image.decode_jpeg(value, channels=3)
+	image = tf.image.resize_images(image, 224, 224)
+	float_image = tf.div(tf.cast(image,tf.float), 255)
+	return float_image
+
+
+def input_pipeline(filenames, batch_size):
+	filename_queue = tf.train.string_input_producer(filenames, num_epochs = num_epochs)
+	file = readfile(filename_queue)
+	capacity = 200
+	min_after_dequeue = 100
+	return tf.train.shuffle_batch([file], batch_size=batch_size,
+		capacity=capacity, min_after_dequeue=min_after_dequeue)
